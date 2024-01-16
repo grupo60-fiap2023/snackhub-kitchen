52888579622dc74a4130068769cb09005dfa8867
Igor Oliveira <igor_villanova@hotmail.com>
2024-01-16 10:32:02 -0300

    Adicionado url para a imagem.

--- app/database.py
+++ app/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "mysql+mysqlconnector://root:123456@localhost:3307/pedidos"
+#URL para acessar o banco pela imagem (produção)
+DATABASE_URL = "mysql+mysqlconnector://root:123456@snackhub-mysql-db-kitchen:3306/pedidos"
+#URL para acessar pelo VSCode (debug)
+#DATABASE_URL = "mysql+mysqlconnector://root:123456@localhost:3307/pedidos"
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
