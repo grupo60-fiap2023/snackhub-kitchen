3025afa2252e4bef0186e2eebf7f6cb9e2e66a42
Igor Oliveira <igor_villanova@hotmail.com>
2024-01-16 10:29:50 -0300

    Removido teste de não encontrar registro. Adicionado coverage

--- tests/unit/test_read.py
+++ tests/unit/test_read.py
@@ -6,13 +6,3 @@ client = TestClient(app)
 def test_findStatusDone_foundOneRegister():
     response = client.get('kitchen/pedidos/4')
     assert response.status_code == 200
-    
-    
-def test_findStatusPreparing_foundNoRegister():
-    response = client.get('kitchen/pedidos/4')
-    assert response.status_code == 200
-    assert response.json() == {
-        "Status": "Success",
-        "Results": 0,
-        "Pedidos": []
-    }
