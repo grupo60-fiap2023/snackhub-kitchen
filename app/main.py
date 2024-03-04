6daac102e56110d5a2a210270c63240a241650e7
Igor Oliveira <igor_villanova@hotmail.com>
2024-03-04 10:22:48 -0300

    removendo mensageria

--- app/main.py
+++ app/main.py
@@ -2,10 +2,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from app import models, pedidos
 from app.database import engine
 from fastapi import FastAPI
-import boto3
-import threading
-from queue_consumer import receive_messages
-from moto import mock_sqs
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -28,16 +24,3 @@ app.include_router(pedidos.router, tags=["Kitchen"], prefix="/kitchen")
 @app.get("/api/healthchecker")
 def root():
     return {"message": "The API is LIVE!!"}
-
-
-aws_access_key_id = ''
-aws_secret_access_key = ''
-region_name = ''
-
-with mock_sqs():
-    sqs = boto3.client('sqs', region_name=region_name, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-    queue_url = 'order_success'
-
-    thread = threading.Thread(target = receive_messages, args=(sqs, queue_url))
-    thread.daemon = True
-    thread.start()
